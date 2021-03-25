eb77042f4703c498aaf395e4142c7f94723d2ef7
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2021-03-25 23:52:41 +0530

    add topdown knapsack code

---
+++ TopDownAproachKnapsackcode.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class TopDownAproachKnapsackcode {
+  static  int knapsack(int[] wt, int[] val, int w,int n ){
+
+        int[][] dp = new int[n+1][w+1];
+
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if( (i==0) && (j==0))
+                    dp[i][j] = 0;
+            }
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<w+1;j++){
+                if(wt[i-1] <= j){
+                    dp[i][j] = Math.max( (val[i-1]+dp[i-1][j-wt[i-1]]) ,(dp[i-1][j]) );
+                }else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp[n][w];
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n=3;
+        int[] wt = {10,20,30};
+        int[] val = {60,100,120};
+        int w = 50;
+
+        System.out.println( knapsack(wt,val,w,n));
+    }
+}
